0c33c1c202bead1b2f0b3e89140adb4b6ac36e57
mmakatri <mmakatri@yahoo.com>
2019-10-20 17:04:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.android.courtcounter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    final int POINTS_FOR_FREE_THROW = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //displayForTeamA(0);
+    }
+
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void resetAll(View v) {
+
+        if((scoreTeamB > 0) || (scoreTeamB > 0)) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Confirmation");
+            builder.setMessage("You will reset all scores !\n" +
+                    "Are you sure ?");
+            // Add the buttons
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User clicked OK button
+                    scoreTeamB = 0;
+                    scoreTeamA = 0;
+                    displayForTeamB(scoreTeamB);
+                    displayForTeamA(scoreTeamA);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+
+                }
+            });
+            // Create the AlertDialog
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }else{
+            displayForTeamB(scoreTeamB);
+            displayForTeamA(scoreTeamA);
+        }
+    }
+
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+
+        scoreTeamB = scoreTeamB + POINTS_FOR_FREE_THROW;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+
+        scoreTeamA = scoreTeamA + POINTS_FOR_FREE_THROW;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
